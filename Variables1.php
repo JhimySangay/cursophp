3ca2b71e7c7780b3bdad883bbb81751a3b5860e6
JhimySangay <jhimysangay@gmail.com>
2017-07-18 17:12:21 -0500

    Variables
    La variable asume el tipo luego de asignarle un valor

---
+++ Variables1.php
@@ -0,0 +1,12 @@
+<?php 
+	$numEntero=10;
+	echo$numEntero;
+	echo "<br>";
+
+	$numDecimal=8.5;
+	echo$numDecimal;
+	echo "<br>";
+
+	$bandera = false;
+	echo"El valor de la variable es: ".$bandera;
+ ?>
